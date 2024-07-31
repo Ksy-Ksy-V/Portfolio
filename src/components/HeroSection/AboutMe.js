1fefd089b1d6557c0b64136ade1f7078d8ae0fb2
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2024-07-31 16:01:24 +0300

    add about me section to home page

---
+++ src/components/HeroSection/AboutMe.js
@@ -0,0 +1,46 @@
+import { Grid, Box, Typography } from '@mui/material';
+
+import aboutMeImg from './../../img/aboutMe.jpg'
+
+const AboutMe = () => {
+    return (
+        <Box sx={{ backgroundColor: 'primary.dark', padding: '2rem', marginTop: '3rem' }}>
+            <Grid container spacing={2}>
+                <Grid item xs={6} sx={{ paddingLeft: 0, marginTop: 2 }}>
+                    <Box
+                        component='img'
+                        src={aboutMeImg}
+                        alt='Hero Section'
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            border: '3px solid',
+                            borderColor: 'primary.dark'
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <Box>
+                        <Typography 
+                            variant='h5' 
+                            color={'primary.main'} 
+                            mt='3rem'
+                        >
+                            {`< About me! >`}
+                        </Typography>
+                        <Typography 
+                            variant='body1' 
+                            color={'primary.background'} 
+                            mt='3rem'
+                        >
+                            {`< Anim ullamco ex amet ea magna duis ut irure cillum nulla. Dolore et anim aute id aute aliqua sint aute ut sint pariatur do ullamco. Mollit anim ex cupidatat ad exercitation minim anim deserunt anim do aliquip. Nulla culpa sint amet consectetur. >`}
+                        </Typography>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
+ 
+export default AboutMe;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,13 @@
 import HeroSection from "../../components/HeroSection/HeroSection";
-import imgAboutMe from "./../../img/imgAboutMe.jpg" 
+import AboutMe from "../../components/HeroSection/AboutMe"
+
 
 const Home = () => {
     return (
         <div className="about-me-page">
             <HeroSection />
 
-            <div className="about-me">
-                <h2>About me</h2>
-                <div className="about-me-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <img 
-                        src={imgAboutMe}
-                        alt="Title"
-                        className="about-me__cover" 
-                    />
-                </div>
-            </div>
+            <AboutMe />
  
 
             <ul className="tech-stack">
